5edb4e6baee88983f42b26f02bd7e063ea917c8c
YubaNeupane <neopaneyb2@gmail.com>
2020-06-13 22:05:05 -0400

    Updates

---
+++ precache-manifest.e5088f9ae9b13551f3b90477900190ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2631c517633ada8f695ea6a1852c75a0",
+    "url": "/Movie-Search/index.html"
+  },
+  {
+    "revision": "8b98fa4b9fbe125e3b74",
+    "url": "/Movie-Search/static/css/main.4a4f81a7.chunk.css"
+  },
+  {
+    "revision": "dfb45f24c9b1dd49c370",
+    "url": "/Movie-Search/static/js/2.1cfbfb3d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Movie-Search/static/js/2.1cfbfb3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b98fa4b9fbe125e3b74",
+    "url": "/Movie-Search/static/js/main.ddf82587.chunk.js"
+  },
+  {
+    "revision": "2869d1168774ecb39a36",
+    "url": "/Movie-Search/static/js/runtime-main.42e9bf57.js"
+  }
+]);
